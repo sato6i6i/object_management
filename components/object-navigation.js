eb121c6ed68e45f833e47e3c8bc4ad93c5384669
sato6i6i <71752804+sato6i6i@users.noreply.github.com>
2021-05-24 18:00:25 +0900

    Add files via upload

--- components/object-navigation.js
+++ components/object-navigation.js
@@ -1,44 +1,46 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import { useStaff } from '../lib/hooks'
 
-const objectNaigation = ({ page }) => {
+
+const ObjectNavigation = ({ page }) => {
   const active = "text-center block border border-white hover:border-gray-200 \
-                  text-white bg-gray-700 py-2 px-4"
+                  text-white bg-gray-700 py-2 px-2"
   const inactive = "text-center block border border-white hover:border-gray-200 \
-                    text-gray-700" hover:bg-gray-700 hover:text-white py-2 px-4"
+                    text-gray-700 hover:bg-gray-700 hover:text-white py-2 px-2"
                     
   return (
     <div className="mb-4">
       <ul className="flex">
-        <li className="flex-1 mr-2">
+        <li className="flex-1 mr-1">
           <Link href="/object/hold_onto">
-            <a className={page == 'hold_onto' ? active : inactive}>—a‚©‚è•i</a>
+            <a className={page == 'hold_onto' ? active : inactive}>é ã‹ã‚Šå“</a>
           </Link>
         </li>
-        <li className="flex-1 mr-2">
+        <li className="flex-1 mr-1">
           <Link href="/object/construction_materials">
-            <a className={page == 'construction_materials' ? active : inactive}>H–Ş—¿</a>
+            <a className={page == 'construction_materials' ? active : inactive}>å·¥äº‹ææ–™</a>
           </Link>
         </li>
-        <li className="flex-1 mr-2">
+        <li className="flex-1 mr-1">
           <Link href="/object/lease_rental_pc">
             <a className={page == 'lease_rental_pc' ? active : inactive}>PC</a>
           </Link>
         </li>
-        <li className="flex-1 mr-2">
+        <li className="flex-1 mr-1">
           <Link href="/object/phone_wifi">
-            <a className={page == 'phone_wifi' ? active : inactive}>Œg‘ÑEwifi</a>
+            <a className={page == 'phone_wifi' ? active : inactive}>æºå¸¯ãƒ»wifi</a>
           </Link>
         </li>
-        <li className="flex-1 mr-2">
+        <li className="flex-1 mr-1">
           <Link href="/object/fixed_asset">
-            <a className={page == 'fixed_asset' ? active : inactive}>ŒÅ’è‘Y</a>
+            <a className={page == 'fixed_asset' ? active : inactive}>å›ºå®šè³‡ç”£</a>
           </Link>
         </li>
-        <li className="flex-1 mr-2">
+        <li className="flex-1 mr-1">
           <Link href="/object/disaster_stockpile">
-            <a className={page == 'disaster_stockpile' ? active : inactive}>ĞŠQ”õ’~•i</a>
+            <a className={page == 'disaster_stockpile' ? active : inactive}>ç½å®³å‚™è“„å“</a>
           </Link>
         </li>
       </ul>
@@ -46,4 +48,4 @@ const objectNaigation = ({ page }) => {
   )
 }
 
-export default objectNaigation
+export default ObjectNavigation
